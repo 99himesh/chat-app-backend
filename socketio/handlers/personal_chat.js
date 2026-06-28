95cef4689888aec38ca8607f9510b3337dd11227
mac <mac@macs-MacBook-Pro.local>
2026-06-29 00:55:57 +0530

    personal message

--- socketio/handlers/personal_chat.js
+++ socketio/handlers/personal_chat.js
@@ -1,10 +1,12 @@
 module.exports=(socket,io)=>{
 
     socket.on("join-room",(roomName)=>{
-        socket.join(roomName)
+        socket.join(roomName);
+        console.log("room joined",roomName);
+        
     })
-     socket.on("personal-message",(data,roomName)=>{        
-        io.emit("personal-recieve-message",{...data,userName:socket.user.name})
+     socket.on("personal-message",({message,roomName})=>{        
+        io.to(roomName).emit("personal-recieve-message",{message,userName:socket.user.name,roomName})
      })
      
 }
