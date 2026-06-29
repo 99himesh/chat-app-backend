ed74b045a27d8cc583d9329ef5ee2623abc12b08
mac <mac@macs-MacBook-Pro.local>
2026-06-29 13:52:12 +0530

    update email

--- socketio/handlers/personal_chat.js
+++ socketio/handlers/personal_chat.js
@@ -2,10 +2,11 @@ module.exports=(socket,io)=>{
 
     socket.on("join-room",(roomName)=>{
         socket.join(roomName);
-        console.log("room joined",roomName);
-        
+        console.log("room joined",roomName)
     })
-     socket.on("personal-message",({message,roomName})=>{        
+     socket.on("personal-message",({message,roomName})=>{
+        console.log(message,roomName,"fgdfgdfgdf");
+                
         io.to(roomName).emit("personal-recieve-message",{message,userName:socket.user.name,roomName})
      })
      
--- socketio/middleware.js
+++ socketio/middleware.js
@@ -10,10 +10,8 @@ module.exports=(io)=>{
         const token=socket.handshake.auth.token;
 
             const user=await jwt.verify(token,process.env.JWT_SECRET_KEY)  ;
-    console.log(user,"dfkjdsfhjsd");
                 
             const userData=await UserModel.findByPk(user.userId);
-            console.log(userData,",djbfhjdsg");
             
             socket.user=userData;         
             next();
